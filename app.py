5ee0355c85d2bbee74f574e6f8c3874c034c7bef
swamini10 <swaminibhandare10@gmail.com>
2025-10-04 00:04:30 +0530

    loginpage

--- app.py
+++ app.py
@@ -1,37 +1,44 @@
-from flask import Flask, request  #  import Flask
+from flask import Flask, request, redirect, url_for,session,Response
 
 app = Flask(__name__)  # Create a Flask application instance
-# creat an object of Flask class
-@app.route("/")  # Define a route for the root
-def home():  # Define a view function for the route
-    return "Hello, World! This is my first flask app "  # Return a simple response
-    
-    #  go to terminal and run the command 
-    # set  virtual environment 
-    # python -m venv venv
-    # .\venv\Scripts\activate
-    # pip install flask
-    # to run the app
-    # flask run
-    
-    
-    # Running on http://127.0.0.1:5000
-    # http comunition protocol
-    # 127.0.0.1 is localhost
-    # 5000 is port number
-    # to stop the server ctrl + c
-@app.route("/about")  # Define a route for /about  # Running on http://127.0.0.1:5000/about
-def about():
-    return "this is about"
+app.secret_key = "supersecretkey" # Set a secret key for session management
 
-@app.route("/contact")  # Define a route for /contact  # Running on http://127.0.0.1:5000/contact
-def contact():
-    return "this is contact"
-
-
-@app.route("/submit",methods =["GET","POST"])
-def submit():
+@app.route("/", methods=["GET", "POST"])
+def login():
     if request.method == "POST":
-        return "You send the data"
-    else:
-        return "You are only viewing the form"
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if username == "swamini" and password == "swamini@13":
+            session["user"] = username #store the user in session
+            return redirect(url_for("welcome"))
+        else:
+            return Response("Invalid credentials! Please try again.", mimetype="text/plain")
+        #  text/html return kare
+        
+    return'''
+    <h2>Login Page</h2>
+    <form method="POST">
+        username: <input type="text" name="username"><br>
+        password: <input type="password" name="password"><br>
+        <input type="submit" value="Login">
+    </form>
+    '''
+    
+    
+    # weclome page after login
+    @app.route("/welcome")
+    def Welcome():
+        if "user" in session:
+            return f'''
+            <h2>Welcome, {session["user"]}!</h2>
+            <a href="{url_for("logout")}">Logout</a>
+            '''
+
+        return redirect(url_for("login"))
+
+# logout route
+@app.route("/logout")
+def logout():
+    session.pop("user", None)# remove user from session
+    return redirect(url_for("login"))
