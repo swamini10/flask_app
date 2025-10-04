23982f2ba126f0138db9d676647c1e169dbad2c3
swamini10 <swaminibhandare10@gmail.com>
2025-10-04 14:31:03 +0530

    updated

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, request, redirect, url_for,session,Response
+from flask import Flask, request, redirect, url_for, session, Response
 
-app = Flask(__name__)  # Create a Flask application instance
-app.secret_key = "supersecretkey" # Set a secret key for session management
+app = Flask(__name__)
+app.secret_key = "supersecretkey"
 
 @app.route("/", methods=["GET", "POST"])
 def login():
@@ -10,13 +10,11 @@ def login():
         password = request.form.get("password")
 
         if username == "swamini" and password == "swamini@13":
-            session["user"] = username #store the user in session
+            session["user"] = username
             return redirect(url_for("welcome"))
         else:
             return Response("Invalid credentials! Please try again.", mimetype="text/plain")
-        #  text/html return kare
-        
-    return'''
+    return '''
     <h2>Login Page</h2>
     <form method="POST">
         username: <input type="text" name="username"><br>
@@ -24,21 +22,20 @@ def login():
         <input type="submit" value="Login">
     </form>
     '''
-    
-    
-    # weclome page after login
-    @app.route("/welcome")
-    def Welcome():
-        if "user" in session:
-            return f'''
-            <h2>Welcome, {session["user"]}!</h2>
-            <a href="{url_for("logout")}">Logout</a>
-            '''
 
-        return redirect(url_for("login"))
+@app.route("/welcome")
+def welcome():
+    if "user" in session:
+        return f'''
+        <h2>Welcome, {session["user"]}!</h2>
+        <a href="{url_for("logout")}">Logout</a>
+        '''
+    return redirect(url_for("login"))
 
-# logout route
 @app.route("/logout")
 def logout():
-    session.pop("user", None)# remove user from session
+    session.pop("user", None)
     return redirect(url_for("login"))
+
+if __name__ == "__main__":
+    app.run(debug=True)
